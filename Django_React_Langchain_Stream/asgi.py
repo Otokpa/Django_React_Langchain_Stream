df31ac678b1f9b2532317f46bb12bcaf2d19e940
Otokpa <maximmoshek@hotmail.com>
2024-03-09 23:32:18 +0100

    Initial commit

---
+++ Django_React_Langchain_Stream/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for Django_React_Langchain_Stream project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import langchain_stream.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_React_Langchain_Stream.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            langchain_stream.routing.websocket_urlpatterns
+        )
+    ),
+})
